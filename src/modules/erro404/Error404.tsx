9db90f8d1f4d158398f9ec4b517ba14f8cc5d981
renatodevv <apazaarenato@gmail.com>
2024-06-10 19:54:46 -0500

    add changes in aside navbar the portfolio personal

--- src/modules/erro404/Error404.tsx
+++ src/modules/erro404/Error404.tsx
@@ -1,5 +1,10 @@
+import './error404.css'
+
 export default function Error404 () {
     return (
-        <h1>Error 404 page not found</h1>
+        <div className='error404'>
+            <h1>Error 404 page not found</h1>
+        </div>
+        
     )
 }
